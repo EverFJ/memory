d13a8692d947f16b31d5370a23d2be5f0649aa82
Francois Janson <jansonfrancois@gmail.com>
2021-08-30 15:22:55 +0200

    creating architecture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "./components/Card";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [
+        { id: 1, color: "yellow", flipped: false },
+        { id: 2, color: "yellow", flipped: false },
+        { id: 3, color: "red", flipped: false },
+        { id: 4, color: "red", flipped: false },
+        { id: 5, color: "blue", flipped: false },
+        { id: 6, color: "blue", flipped: false },
+        { id: 7, color: "orange", flipped: false },
+        { id: 8, color: "orange", flipped: false },
+      ],
+      moves: 0,
+      pairs: 0,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Memory Game </h1>
+      </>
+    );
+  }
 }
 
 export default App;
