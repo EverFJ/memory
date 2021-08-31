18e0168533c9decebbfa486b3c5e88a73336b829
Francois Janson <jansonfrancois@gmail.com>
2021-08-31 12:29:28 +0200

    cleaning

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,10 +5,14 @@ class Card extends React.Component {
     console.log(this.props);
     let { color, isFlipped, onClick, id } = this.props;
 
+    const style = {
+      transform: `${!isFlipped ? "rotateX(180deg)" : ""} `,
+    };
     return (
       <>
         <div
           className={`card ${isFlipped && "flipped"} ${color}`}
+          style={style}
           onClick={() => {
             onClick(id);
           }}
