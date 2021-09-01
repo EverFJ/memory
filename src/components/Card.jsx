6f113cb27b4a34e7eac992961e01d4aebfe5e365
Francois Janson <jansonfrancois@gmail.com>
2021-09-01 12:00:09 +0200

    rotate finally working

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,16 +4,12 @@ class Card extends React.Component {
   render() {
     let { color, isFlipped, onClick, id } = this.props;
 
-    const style = {
-      transform: `${!isFlipped ? "rotateX(180deg)" : ""} `,
-    };
     return (
       <>
         <div
           className={`card ${isFlipped && "flipped"} ${
             !isFlipped && "rotate"
           } ${color}`}
-          style={style}
           onClick={() => {
             onClick(id);
           }}
